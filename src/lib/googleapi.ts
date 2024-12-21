eb651e45c9769ac71c775a8b0065a0bcda37a063
Denzel Co <denzel_adrian_co@dlsu.edu.ph>
2024-12-21 17:54:59 +0800

    changed auth

--- src/lib/googleapi.ts
+++ src/lib/googleapi.ts
@@ -2,9 +2,13 @@ import { google } from "googleapis";
 import { IShoe } from "@/models/Product";
 
 export async function getGoogleSheetsData() {
-  const auth = new google.auth.JWT({
-    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    key: process.env.GOOGLE_PRIVATE_KEY,
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      client_id: process.env.CLIENT_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
+    },
+
     scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
   });
 
