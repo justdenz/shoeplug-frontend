f33b0984017ecca7f5a3200c8f4fb4e21e60fdaf
Denzel Co <denzel_adrian_co@dlsu.edu.ph>
2025-07-22 00:33:16 +0800

    changed product card width

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -22,35 +22,7 @@ const ProductCard: React.FC<ProductCardProps> = (props: ProductCardProps) => {
     return <div className="text-green-500">Brand New</div>;
   };
   return (
-    // <div className="w-2xs">
-    //   <div
-    //     className="rounded relative w-60 h-80 overflow-hidden"
-    //     key={props.product.shoe_id}
-    //   >
-    //     <CldImage
-    //       priority={true}
-    //       fill={true}
-    //       sizes="25vw"
-    //       src={props.product.image_url}
-    //       alt="Logo"
-    //       className="object-cover rounded-md"
-    //     />
-    //   </div>
-    //   <div className="w-5/6">
-    //     <div className="flex flex-row justify-between font-bold">
-    //       <div className="text-ellipsis overflow-hidden text-base">
-    //         {props.product.model}
-    //       </div>
-    //     </div>
-    //     <div className="font-medium">{"Size: " + props.product.size}</div>
-    //     <div className="flex flex-row justify-between">
-    //       <div className="font-bold">{"₱" + props.product.price}</div>
-    //       {conditionElement()}
-    //     </div>
-    //   </div>
-    // </div>
-
-    <div className="w-[450px] h-full bg-white rounded-lg shadow p-2 grid grid-rows-[1/5_auto_auto_auto] gap-1">
+    <div className="w-[350px] h-full bg-white rounded-lg shadow p-2 grid grid-rows-[1/5_auto_auto_auto] gap-1">
       {/* Image */}
       <div className="justify-items-center">
         <CldImage
