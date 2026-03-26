80a625fe1b3cdc040855ab1ef989128f29e84977
Denzel <denz3550@gmail.com>
2026-03-26 22:25:36 +0800

    added resource

--- src/models/resource.ts
+++ src/models/resource.ts
@@ -1,5 +1,15 @@
 export const PAGE_SIZE = 10;
-export const conditions = ["BRAND_NEW", "GOOD_AS_NEW", "USED"];
+export const conditions = ["BRAND_NEW", "USED"];
 export const FACEBOOK_URL =
   "https://www.facebook.com/profile.php?id=100054236060642";
 export const IG_URL = "https://www.instagram.com/shoeplug.ph/";
+export const COLUMNS = {
+  shoe_id: 0,
+  date_bought: 1,
+  model: 2,
+  condition: 3,
+  size: 4,
+  price: 5,
+  status: 6,
+  brand: 7,
+};
